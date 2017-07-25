853e6ee2142c72eff72b851632b2363e636306d4
Ahmed Fasih <wuzzyview@gmail.com>
2017-07-25 19:05:03 -0400

    Encrypting content, not yet submitting

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ const SERVER = 'https://sync.standardnotes.org';
 const COST = 100e3;
 
 import { readFileSync, writeFile } from "fs";
-import { randomBytes, createHash, pbkdf2, createHmac } from 'crypto';
+import { randomBytes, createHash, pbkdf2, createHmac, createCipheriv } from 'crypto';
 import fetch from "node-fetch";
 const promisify = require("es6-promisify");
 const pbkdf2Async = promisify(pbkdf2);
@@ -84,6 +84,71 @@ async function login(email: string, userpass: string) {
     // Get a JWT in response
     const authResponse = await signinResponse.json();
     console.log('JWT', authResponse);
+    return { authResponse, mk, ak };
+}
+
+async function cipherContent(cleartext: string, encryption_key: Buffer) {
+    const iv = await randomBytesAsync(128 / 8);
+    const cipher = createCipheriv('aes-256-cbc', encryption_key, iv);
+    let ciphertext = cipher.update(cleartext, 'utf8', 'base64');
+    ciphertext += cipher.final('base64');
+    return { iv: iv.toString('hex'), ciphertext };
+}
+
+async function encrypt002(cleartext: string, uuid: string, encryption_key: Buffer, authentication_key: Buffer) {
+    const ciphered = await cipherContent(cleartext, encryption_key);
+    const string_to_auth = ['002', ciphered.iv, uuid, ciphered.ciphertext].join(':');
+
+    const hmac = createHmac('sha256', authentication_key)
+    hmac.update(string_to_auth);
+    const auth_hash = hmac.digest('hex');
+    const result = ["002", auth_hash, ciphered.iv, uuid, ciphered.ciphertext].join(":")
+    return result;
+}
+
+interface Item {
+    uuid: string;
+    content: any; // Any before encryption, string after
+    content_type: string;
+    enc_item_key: string; // None before encryption, string after
+    deleted: boolean;
+    created_at: Date;
+    updated_at: Date;
+}
+
+interface WebToken {
+    user: any;
+    token: string;
+}
+
+async function sync(origItems: Item[], mk: string, ak: string, jwt: WebToken) {
+    let items: Item[] = [];
+    try {
+        for (const origItem of origItems) {
+            // Encryption key
+            const item_ek: Buffer = await randomBytesAsync(512 / 8 / 2);
+            // Authentication key
+            const item_ak: Buffer = await randomBytesAsync(512 / 8 / 2);
+            // Combined key
+            const item_key = Buffer.concat([item_ek, item_ak]);
+
+            let newItem: Item = Object.assign(
+                origItem,
+                {
+                    content: await encrypt002(JSON.stringify(origItem), origItem.uuid, item_ek, item_ak),
+                    enc_item_key: await encrypt002(
+                        item_key.toString('utf8'),
+                        origItem.uuid,
+                        Buffer.from(mk, 'hex'),
+                        Buffer.from(ak, 'hex'))
+                });
+            items.push(newItem);
+        }
+    }
+    catch (e) {
+        console.error('Error', e)
+    }
+    console.log(items);
 }
 
 // register('foo@test.com', 'testing', false)
@@ -96,10 +161,23 @@ async function login(email: string, userpass: string) {
 Server replied with 200
 */
 
-login('foo@test.com', 'testing');
+const authResponse = login('foo@test.com', 'testing');
 /*
 JWT { user:
    { uuid: '38f41c3e-cc90-4384-a969-4337aa160987',
      email: 'foo@test.com' },
   token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3V1aWQiOiIzOGY0MWMzZS1jYzkwLTQzODQtYTk2OS00MzM3YWExNjA5ODciLCJwd19oYXNoIjoiNGU5MjVkM2E1ZWRjNjgwMjVhNzNhYTkwZmExNTQ5NjIyMzYzZjFhM2ExYTFkMzNkMmU2NzY1Mjg0NDJlYzgzZSJ9.BbnQQq1Q-sIys_mdU7OObzG4cN3eRFElXFGzFUH-SPU' }
 */
+
+function makeItem(uuid: string,
+    content: any,
+    content_type: string,
+    deleted: boolean,
+    created_at: Date,
+    updated_at: Date): Item {
+    return { uuid, content, content_type, deleted, created_at, updated_at: updated_at || new Date(), enc_item_key: null }
+}
+
+const item: Item = makeItem('1234', { hi: "there", bye: "now" }, 'testing', false, new Date(), new Date());
+
+authResponse.then(({ authResponse, mk, ak }) => sync([item], mk, ak, authResponse));
